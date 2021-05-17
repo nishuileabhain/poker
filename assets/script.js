355d6e09ad95082d98641e082c8f75837565ede7
nishuileabhain <nishuileabhain@gmail.com>
2021-05-17 07:29:04 +0000

    added drag and drop functionality in script

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,18 @@ draggables.forEach(draggable => {
   draggable.addEventListener('dragstart', () => {
     draggable.classList.add('dragging')
   })
+//what happens when you let you of the item being dragged
 
   draggable.addEventListener('dragend', () => {
     draggable.classList.remove('dragging')
   })
 })
+
+//highlights which container it is currently hovering over
+containers.forEach(container => {
+  container.addEventListener('dragover', () => {
+    const draggable = document.querySelector('.dragging')
+      container.appendChild(draggable)
+  })
+})
+
