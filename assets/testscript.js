ae93aedb1941d6569202caf45fa47f99fd02b1e1
nishuileabhain <nishuileabhain@gmail.com>
2021-06-02 11:20:30 +0000

    reclassified divs and ids in question one and added images and wrong answer feedback

--- assets/testscript.js
+++ assets/testscript.js
@@ -1,28 +1,29 @@
-
+let countCorrect = 0;
+let countIncorrect = 0;
+  
+  
   //    The function evaluates the answer and displays result
   function displayAnswer1() {
-    if (document.getElementById('option-11').checked) {
-      document.getElementById('block-11').style.border = '3px solid limegreen'
-      document.getElementById('result-11').style.color = 'limegreen'
-      document.getElementById('result-11').innerHTML = 'Correct!'
+    if (document.getElementById('option-1A').checked) {
+        document.getElementById("block-1A").classList.add("correct");
+        document.getElementById('result-1A').innerHTML = 'Correct!'
     }
-    if (document.getElementById('option-12').checked) {
-      document.getElementById('block-12').style.border = '3px solid red'
-      document.getElementById('result-12').style.color = 'red'
-      document.getElementById('result-12').innerHTML = 'Incorrect!'
+    if (document.getElementById('option-1B').checked) {
+      document.getElementById("block-1B").classList.add("wrong");
+      document.getElementById('result-1B').innerHTML = 'Incorrect! A royal flush is made up of an A, K, Q, J & 10'
       showCorrectAnswer1()
     }
-    if (document.getElementById('option-13').checked) {
-      document.getElementById('block-13').style.border = '3px solid red'
-      document.getElementById('result-13').style.color = 'red'
-      document.getElementById('result-13').innerHTML = 'Incorrect!'
+    if (document.getElementById('option-1C').checked) {
+      //document.getElementById('block-13').style.border = '3px solid red'
+      //document.getElementById('result-13').style.color = 'red'
+      document.getElementById('result-1C').innerHTML = 'Incorrect! A royal flush is made up of an A, K, Q, J & 10'
+      document.getElementById("block-1C").classList.add("wrong");
       showCorrectAnswer1()
     }
-    if (document.getElementById('option-14').checked) {
-      document.getElementById('block-14').style.border = '3px solid red'
-      document.getElementById('result-14').style.color = 'red'
-      document.getElementById('result-14').innerHTML = 'Incorrect!'
-      showCorrectAnswer1()
+    if (document.getElementById('option-1D').checked) {
+        document.getElementById("block-1D").classList.add("wrong");
+        document.getElementById('result-1D').innerHTML = 'Incorrect! A royal flush is made up of an A, K, Q, J & 10'
+        showCorrectAnswer1()
     }
   }
   // the functon displays the link to the correct answer
