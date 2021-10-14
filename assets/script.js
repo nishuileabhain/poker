8528813a612871f8e4aed0de57f589db6d51b643
nishuileabhain <nishuileabhain@gmail.com>
2021-10-14 21:21:39 +0000

    add alt tags to all images

--- assets/script.js
+++ assets/script.js
@@ -1,105 +1,106 @@
 /*jshint esversion: 6 */
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('questionID')
-const answerButtonsElement = document.getElementById('answer-buttons')
-const responsePic = document.getElementById('responsePic')
-const responseText = document.getElementById('responseText')
-const finalScore = document.getElementById('finalScore')
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('questionID');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const responsePic = document.getElementById('responsePic');
+const responseText = document.getElementById('responseText');
+const finalScore = document.getElementById('finalScore');
 
 
-let shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex;
 let correctAnswers = 0;
 
-startButton.addEventListener('click', startGame) //user clicks start button
+startButton.addEventListener('click', startGame); //user clicks start button
 nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
-  startButton.classList.add('hide')//start button and score are hidden until the end
-  finalScore.classList.add('hide')
+  startButton.classList.add('hide');//start button and score are hidden until the end
+  finalScore.classList.add('hide');
 
-  shuffledQuestions = questionList.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
+  shuffledQuestions = questionList.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
     correctAnswers = 0;
     //console.log("correct answers is "+correctAnswers)
-  questionContainerElement.classList.remove('hide') //after the start button is clicked it will disappear
-  setNextQuestion()
+  questionContainerElement.classList.remove('hide'); //after the start button is clicked it will disappear
+  setNextQuestion();
 }
 
 function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) { //this is the question object as defined below
-  questionElement.innerHTML = question.question
+  questionElement.innerHTML = question.question;
   question.answers.forEach(answer => { //built-in forEach() method calls a function once for each element in the answers array
-    const button = document.createElement('button')
-    button.innerHTML = answer.texty
-    button.classList.add('btn')
+    const button = document.createElement('button');
+    button.innerHTML = answer.texty;
+    button.classList.add('btn');
     if (answer.correct) {
-      button.dataset.correct = answer.correct
+      button.dataset.correct = answer.correct;
     }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
 }
 
 // for showing the next question, not resetting the whole quiz
 
 function resetState() {
   //clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  responsePic.classList.add('hide')
-  responseText.classList.add('hide')
+  nextButton.classList.add('hide');
+  responsePic.classList.add('hide');
+  responseText.classList.add('hide');
   responsePic.src = "assets/images/correct.jpg";
-  responseText.innerHTML = "CORRECT!"
+  responseText.innerHTML = "CORRECT!";
 
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
 
 function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
   //setStatusClass(document.body, correct)
-  selectedButton.classList.add('selectedButton')
+  selectedButton.classList.add('selectedButton');
   //Array.from(answerButtonsElement.children).forEach(button => {
   //  setStatusClass(button, button.dataset.correct)//changes button colours
- // })
+  // })
 
-  responsePic.classList.remove('hide')
-  responseText.classList.remove('hide')
+  responsePic.classList.remove('hide');
+  responseText.classList.remove('hide');
 
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+    nextButton.classList.remove('hide');
   } else {
-    startButton.innerHTML = 'Restart'
-    startButton.classList.remove('hide')
-    finalScore.classList.remove('hide')
-    finalScore.innerHTML = "You got "+correctAnswers+" right!";
+    startButton.innerHTML = 'Restart';
+    startButton.classList.remove('hide');
+    finalScore.classList.remove('hide');
+    finalScore.innerHTML = "You got " + correctAnswers + " right!";
   }
-    if (selectedButton.dataset = correct) {
-        selectedButton.classList.add('correct')
-        correctAnswers++
-        //console.log("correct answers is now "+correctAnswers)
+  if (selectedButton.dataset == correct) {
+    selectedButton.classList.add('correct');
+    correctAnswers++;
+    //console.log("correct answers is now "+correctAnswers)
 
-    }
-    else{
-      selectedButton.classList.add('wrong')
-      responsePic.src = "assets/images/wrong.jpg";
-      responseText.innerHTML = 'WRONG!'
-      //console.log("correct answers is still only "+correctAnswers)
-      Array.from(answerButtonsElement.children).forEach(button => {
-          if(button.dataset.correct)//changes button colours
-          {button.classList.add('missed')}
-        })
-    }
+  } else {
+    selectedButton.classList.add('wrong');
+    responsePic.src = "assets/images/wrong.jpg";
+    responseText.innerHTML = 'WRONG!';
+    //console.log("correct answers is still only "+correctAnswers)
+    Array.from(answerButtonsElement.children).forEach(button => {
+      if (button.dataset.correct); //changes button colours
+      {
+        button.classList.add('missed');
+      }
+    });
+  }
 }
 
 //turns all the buttons colours depending whether they are correct
@@ -114,10 +115,10 @@ function selectAnswer(e) {
   //}
 //}
 
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-}
+//function clearStatusClass(element) {
+//  element.classList.remove('correct');
+//  element.classList.remove('wrong');
+//}
 
 //array of question elements
 const questionList = [
@@ -170,7 +171,7 @@ const questionList = [
     ]
   }
   
-] //EO questions list
+]; //EO questions list
 
 
 
