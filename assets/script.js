57895c0b084d243114f8563d39d3c6dec1285327
nishuileabhain <nishuileabhain@gmail.com>
2021-10-14 20:52:50 +0000

    add ES6 comment for jshint testing

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
